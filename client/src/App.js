8e3e7914e9ac6b5be11595e16b88ed76c37cf47a
Ricardo <ricksolo88@gmail.com>
2019-07-22 18:16:14 -0500

    Nav bar is showing, I think?

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
-
+import { Route } from "react-router-dom";
 import SavedList from './Movies/SavedList';
+import MovieList from "./Movies/MovieList";
+import Movie from "./Movies/Movie";
+
 
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
@@ -10,12 +13,11 @@ const App = () => {
   };
 
   return (
-    <Router>
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <Route exact path="../src/Movies/MovieList.js" component={MovieList} />
+      <Route path="/movies/" component={Movie} />
     </div>
-    </Router>
   );
 };
 
