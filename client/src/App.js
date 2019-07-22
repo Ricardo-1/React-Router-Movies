d507566a63a6b69a59b6dc5c95fc6cbd9e8fb5f0
Ricardo <ricksolo88@gmail.com>
2019-07-22 16:05:44 -0500

    PR

--- client/src/App.js
+++ client/src/App.js
@@ -10,10 +10,12 @@ const App = () => {
   };
 
   return (
+    <Router>
     <div>
       <SavedList list={savedList} />
       <div>Replace this Div with your Routes</div>
     </div>
+    </Router>
   );
 };
 
